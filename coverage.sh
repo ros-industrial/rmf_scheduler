23bd65e0336f6be4ca95289b1201d37bd6a2bb61
Chen Bainian <chenbn@artc.a-star.edu.sg>
2025-03-28 02:36:22 +0000

    Ignore examples in coverage report (!6)
    Signed-off-by: Chen Bainian <chenbn@artc.a-star.edu.sg>

--- coverage.sh
+++ coverage.sh
@@ -20,7 +20,7 @@ fi
 
 package_name=${CI_PROJECT_NAME:-"rmf2_scheduler"}
 
-ignored_files="*/test/*"
+ignored_files=("*/test/*" "*/examples/*")
 
 # Install LCOV
 if [ "$(dpkg-query -W -f='${Status}' lcov 2>/dev/null | grep -c 'ok installed')" -eq 0 ];
@@ -46,7 +46,7 @@ lcov --add-tracefile initial_coverage.info \
 lcov --extract coverage.info "$(pwd)/src/$package_name/*" \
      --output-file coverage.info "${branch_command[@]}" | grep -ve "^Extracting"
 # Filter out ignored files
-lcov --remove coverage.info "$ignored_files" \
+lcov --remove coverage.info "${ignored_files[@]}" \
      --output-file coverage.info "${branch_command[@]}" | grep -ve "^removing"
 if [ "$1" = "ci" ]; then
   # Some sed magic to remove identifiable absolute path
